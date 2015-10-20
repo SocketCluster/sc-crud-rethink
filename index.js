2d53f0380c12f09121dc0ceb03dc243f545f3ac5
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-10-20 23:10:30 +1100

    Polished up everything

--- index.js
+++ index.js
@@ -14,46 +14,15 @@ var SCCRUDRethink = function (worker, options) {
 
   this.thinky = thinky(this.options.thinkyOptions);
 
+  this.channelPrefix = 'crud>';
+
   if (!this.options.defaultPageSize) {
     this.options.defaultPageSize = 10;
   }
 
-  this.modelFieldViewMap = {};
-
   Object.keys(this.schema).forEach(function (modelName) {
     var modelSchema = self.schema[modelName];
     self.models[modelName] = self.thinky.createModel(modelName, modelSchema.fields);
-
-    var viewsMap = modelSchema.views;
-
-    var fieldViewModel = {};
-    self.modelFieldViewMap[modelName] = fieldViewModel;
-
-    _.forOwn(viewsMap, function (viewData, viewName) {
-      var filter = viewData.filter;
-      if (filter) {
-        var filterFields = filter.affectedFields;
-
-        filterFields.forEach(function (fieldName) {
-          if (fieldViewModel[fieldName] == null) {
-            fieldViewModel[fieldName] = {};
-          }
-          fieldViewModel[fieldName][viewName] = true;
-        });
-      }
-
-      var order = viewData.order;
-      if (order) {
-        var orderFields = order.affectedFields;
-
-        orderFields.forEach(function (fieldName) {
-          if (fieldViewModel[fieldName] == null) {
-            fieldViewModel[fieldName] = {};
-          }
-          fieldViewModel[fieldName][viewName] = true;
-        });
-      }
-    });
   });
 
   this.scServer.on('_handshake', function (socket) {
@@ -61,61 +30,36 @@ var SCCRUDRethink = function (worker, options) {
   });
 };
 
-SCCRUDRethink.prototype._getAffectedViews = function (type, field) {
-  var affectedViews = [];
-
-  if (field) {
-    var modelFieldMap = this.modelFieldViewMap[type];
-    if (modelFieldMap) {
-      var fieldViewMap = modelFieldMap[field];
-      _.forOwn(fieldViewMap, function (value, viewName) {
-        affectedViews.push(viewName);
-      });
-    }
-  } else {
-    var modelSchema = this.schema[type] || {};
-    _.forOwn(modelSchema.views, function (value, viewName) {
-      affectedViews.push(viewName);
-    });
-  }
-  return affectedViews;
-};
-
 SCCRUDRethink.prototype._getViewMetaData = function (type, view) {
   var modelViews = this.schema[type].views || {};
   var viewSchema = modelViews[view] || {};
 
   return {
-    filter:  viewSchema.filter || {},
-    order: viewSchema.order || {}
+    filter:  viewSchema.filter,
+    order: viewSchema.order
   };
 };
 
-SCCRUDRethink.prototype._constructOrderedFilteredRethinkQuery = function (ModelClass, query, view) {
-  var viewMetaData = this._getViewMetaData(query.type, view);
+SCCRUDRethink.prototype._constructOrderedFilteredRethinkQuery = function (ModelClass, type, view, predicateData) {
+  var viewMetaData = this._getViewMetaData(type, view);
   var rethinkQuery = ModelClass;
 
-  var filterFn = viewMetaData.filter.predicateFactory;
+  var sanitizedPredicateData;
+  if (predicateData == undefined) {
+    sanitizedPredicateData = null;
+  } else {
+    sanitizedPredicateData = predicateData;
+  }
+
+  var filterFn = viewMetaData.filter;
   if (filterFn) {
-    var sanitizedFilterData;
-    if (query.filterData == undefined) {
-      sanitizedFilterData = null;
-    } else {
-      sanitizedFilterData = query.filterData;
-    }
-    var filter = filterFn(this.thinky.r, sanitizedFilterData);
+    var filter = filterFn(this.thinky.r, sanitizedPredicateData);
     rethinkQuery = rethinkQuery.filter(filter);
   }
 
-  var orderFn = viewMetaData.order.predicateFactory;
+  var orderFn = viewMetaData.order;
   if (orderFn) {
-    var sanitizedOrderData;
-    if (query.orderData == undefined) {
-      sanitizedOrderData = null;
-    } else {
-      sanitizedOrderData = query.orderData;
-    }
-    var order = orderFn(this.thinky.r, sanitizedOrderData);
+    var order = orderFn(this.thinky.r, sanitizedPredicateData);
     rethinkQuery = rethinkQuery.orderBy(order);
   }
   return rethinkQuery;
@@ -127,38 +71,43 @@ SCCRUDRethink.prototype._getDocumentViewOffsets = function (documentId, query, c
 
   if (ModelClass) {
 
-    var affectedViews = this._getAffectedViews(query.type, query.field);
     var tasks = [];
+    var optimizationMap = query.optimization;
 
-    affectedViews.forEach(function (viewName) {
-      tasks.push(function (cb) {
-        var rethinkQuery = self._constructOrderedFilteredRethinkQuery(ModelClass, query, viewName);
+    if (optimizationMap == null) {
+      callback(null, {});
+    } else {
+      _.forOwn(optimizationMap, function (predicateData, viewName) {
+        tasks.push(function (cb) {
+          var rethinkQuery = self._constructOrderedFilteredRethinkQuery(ModelClass, query.type, viewName, predicateData);
 
-        rethinkQuery.offsetsOf(self.thinky.r.row('id').eq(documentId)).execute(function (err, documentOffsets) {
-          if (err) {
-            cb(err);
-          } else {
-            cb(null, {
-              view: viewName,
-              id: documentId,
-              offset: (documentOffsets && documentOffsets.length) ? documentOffsets[0] : null
-            });
-          }
+          rethinkQuery.offsetsOf(self.thinky.r.row('id').eq(documentId)).execute(function (err, documentOffsets) {
+            if (err) {
+              cb(err);
+            } else {
+              cb(null, {
+                view: viewName,
+                id: documentId,
+                predicateData: predicateData,
+                offset: (documentOffsets && documentOffsets.length) ? documentOffsets[0] : null
+              });
+            }
+          });
         });
       });
-    });
 
-    async.parallel(tasks, function (err, results) {
-      var viewOffetMap = {};
-      if (!err) {
-        results.forEach(function (viewOffset) {
-          if (viewOffset != null) {
-            viewOffetMap[viewOffset.view] = viewOffset;
-          }
-        });
-      }
-      callback(err, viewOffetMap);
-    });
+      async.parallel(tasks, function (err, results) {
+        var viewOffetMap = {};
+        if (!err) {
+          results.forEach(function (viewOffset) {
+            if (viewOffset != null) {
+              viewOffetMap[viewOffset.view] = viewOffset;
+            }
+          });
+        }
+        callback(err, viewOffetMap);
+      });
+    }
   }
 };
 
@@ -168,7 +117,7 @@ SCCRUDRethink.prototype._isWithinRealtimeBounds = function (offset) {
 
 SCCRUDRethink.prototype._formatErrorResponse = function (err) {
   if (err == null) {
-    return 'Unknown CRUD error';
+    return err;
   }
   if (err.message) {
     return err.message;
@@ -180,6 +129,10 @@ SCCRUDRethink.prototype._formatErrorResponse = function (err) {
 };
 
 
+SCCRUDRethink.prototype._getViewChannelName = function (viewName, predicateData, type) {
+  return this.channelPrefix + viewName + '(' + JSON.stringify(predicateData) + '):' + type;
+};
+
 SCCRUDRethink.prototype.create = function (query, callback) {
   var self = this;
 
@@ -189,19 +142,26 @@ SCCRUDRethink.prototype.create = function (query, callback) {
     if (err) {
       callback && callback(self._formatErrorResponse(err));
     } else {
-      self._getDocumentViewOffsets(result.id, query, function (err, viewOffets) {
-        if (!err) {
-          _.forOwn(viewOffets, function (offsetData, viewName) {
-            if (self._isWithinRealtimeBounds(offsetData.offset)) {
-              self.scServer.global.publish(viewName + ':' + query.type, {
-                type: 'create',
-                id: offsetData.id,
-                offset: offsetData.offset
-              });
-            }
-          });
-        }
-      });
+      if (query.optimization == null) {
+        self.scServer.global.publish(self.channelPrefix + query.type, {
+          type: 'create',
+          id: result.id
+        });
+      } else {
+        self._getDocumentViewOffsets(result.id, query, function (err, viewOffsets) {
+          if (!err) {
+            _.forOwn(viewOffsets, function (offsetData, viewName) {
+              if (self._isWithinRealtimeBounds(offsetData.offset)) {
+                self.scServer.global.publish(self._getViewChannelName(viewName, offsetData.predicateData, query.type), {
+                  type: 'create',
+                  id: result.id,
+                  offset: offsetData.offset
+                });
+              }
+            });
+          }
+        });
+      }
       callback && callback(err, result.id);
     }
   };
@@ -266,7 +226,7 @@ SCCRUDRethink.prototype.read = function (query, callback) {
     if (query.id) {
       ModelClass.get(query.id).run(loadedHandler);
     } else {
-      var rethinkQuery = self._constructOrderedFilteredRethinkQuery(ModelClass, query, query.view);
+      var rethinkQuery = self._constructOrderedFilteredRethinkQuery(ModelClass, query.type, query.view, query.predicateData);
 
       var tasks = [];
 
@@ -299,52 +259,84 @@ SCCRUDRethink.prototype.read = function (query, callback) {
   }
 };
 
+SCCRUDRethink.prototype._getDiffMap = function (change) {
+  if (change == null) {
+    return {};
+  }
+
+  var diffs = {};
+
+  var oldObject = change.old_val;
+  var newObject = change.new_val;
+
+  _.forOwn(oldObject, function (value, key) {
+    if (!newObject.hasOwnProperty(key)) {
+      diffs[key] = null;
+    }
+  });
+  _.forOwn(newObject, function (value, key) {
+    if (newObject[key] != oldObject[key]) {
+      diffs[key] = value;
+    }
+  });
+  return diffs;
+};
+
 SCCRUDRethink.prototype.update = function (query, callback) {
   var self = this;
 
   var savedHandler = function (err, oldViewOffsets, queryResult) {
     if (!err) {
       if (query.field) {
-        self.scServer.global.publish(query.type + '/' + query.id + '/' + query.field, {
+        self.scServer.global.publish(self.channelPrefix + query.type + '/' + query.id + '/' + query.field, {
           type: 'update',
           value: query.value
         });
       } else {
-        _.forOwn(queryResult, function (value, field) {
-          self.scServer.global.publish(query.type + '/' + query.id + '/' + field, {
+        var diffMap = self._getDiffMap(queryResult.changes[0]);
+
+        _.forOwn(diffMap, function (value, field) {
+          self.scServer.global.publish(self.channelPrefix + query.type + '/' + query.id + '/' + field, {
             type: 'update',
             value: value
           });
         });
       }
 
-      self._getDocumentViewOffsets(query.id, query, function (err, newViewOffsets) {
-        if (!err) {
-          _.forOwn(newViewOffsets, function (newOffsetData, viewName) {
-            var oldOffsetData = oldViewOffsets[viewName] || {};
-            newOffsetData = newOffsetData || {};
+      if (query.optimization == null) {
+        self.scServer.global.publish(self.channelPrefix + query.type, {
+          type: 'update',
+          id: query.id
+        });
+      } else {
+        self._getDocumentViewOffsets(query.id, query, function (err, newViewOffsets) {
+          if (!err) {
+            _.forOwn(newViewOffsets, function (newOffsetData, viewName) {
+              var oldOffsetData = oldViewOffsets[viewName] || {};
+              newOffsetData = newOffsetData || {};
 
-            if (oldOffsetData.offset != newOffsetData.offset) {
-              if (self._isWithinRealtimeBounds(oldOffsetData.offset)) {
-                self.scServer.global.publish(viewName + ':' + query.type, {
-                  type: 'update',
-                  freshness: 'old',
-                  id: query.id,
-                  offset: oldOffsetData.offset
-                });
+              if (oldOffsetData.offset != newOffsetData.offset) {
+                if (self._isWithinRealtimeBounds(oldOffsetData.offset)) {
+                  self.scServer.global.publish(self._getViewChannelName(viewName, oldOffsetData.predicateData, query.type), {
+                    type: 'update',
+                    freshness: 'old',
+                    id: query.id,
+                    offset: oldOffsetData.offset
+                  });
+                }
+                if (self._isWithinRealtimeBounds(newOffsetData.offset)) {
+                  self.scServer.global.publish(self._getViewChannelName(viewName, newOffsetData.predicateData, query.type), {
+                    type: 'update',
+                    freshness: 'new',
+                    id: query.id,
+                    offset: newOffsetData.offset
+                  });
+                }
               }
-              if (self._isWithinRealtimeBounds(newOffsetData.offset)) {
-                self.scServer.global.publish(viewName + ':' + query.type, {
-                  type: 'update',
-                  freshness: 'new',
-                  id: query.id,
-                  offset: newOffsetData.offset
-                });
-              }
-            }
-          });
-        }
-      });
+            });
+          }
+        });
+      }
     }
     callback && callback(self._formatErrorResponse(err));
   };
@@ -361,9 +353,11 @@ SCCRUDRethink.prototype.update = function (query, callback) {
       if (query.field == 'id') {
         savedHandler('Cannot modify the id field of an existing document');
       } else {
-        tasks.push(function (cb) {
-          self._getDocumentViewOffsets(query.id, query, cb);
-        });
+        if (query.optimization != null) {
+          tasks.push(function (cb) {
+            self._getDocumentViewOffsets(query.id, query, cb);
+          });
+        }
 
         tasks.push(function (cb) {
           ModelClass.get(query.id).run().then(function (instance) {
@@ -377,14 +371,16 @@ SCCRUDRethink.prototype.update = function (query, callback) {
         if (query.value.id == null) {
           query.value.id = query.id;
         }
-
-        tasks.push(function (cb) {
-          self._getDocumentViewOffsets(query.id, query, cb);
-        });
+        if (query.optimization != null) {
+          tasks.push(function (cb) {
+            self._getDocumentViewOffsets(query.id, query, cb);
+          });
+        }
 
         tasks.push(function (cb) {
           // Replace the whole document
-          ModelClass.get(query.id).replace(query.value).run(cb);
+          // ModelClass.get(query.id).replace(query.value, {returnChanges: true}).execute(cb);
+          self.thinky.r.table(query.type).get(query.id).replace(query.value, {returnChanges: true}).run(cb);
         });
       } else {
         savedHandler('Cannot replace document with a primitive - Must be an object');
@@ -395,7 +391,11 @@ SCCRUDRethink.prototype.update = function (query, callback) {
         if (err) {
           savedHandler(err);
         } else {
-          savedHandler(null, results[0], results[1]);
+          if (query.optimization == null) {
+            savedHandler(null, null, results[0]);
+          } else {
+            savedHandler(null, results[0], results[1]);
+          }
         }
       });
     }
@@ -405,22 +405,37 @@ SCCRUDRethink.prototype.update = function (query, callback) {
 SCCRUDRethink.prototype.delete = function (query, callback) {
   var self = this;
 
-  var deletedHandler = function (err, result) {
+  var deletedHandler = function (err, viewOffsets, result) {
     if (!err) {
       if (query.field) {
-        self.scServer.global.publish(query.type + '/' + query.id + '/' + query.field, {
-          type: 'delete',
-          value: query.value
+        self.scServer.global.publish(self.channelPrefix + query.type + '/' + query.id + '/' + query.field, {
+          type: 'delete'
         });
       } else {
         var change = result.changes[0] || {};
         var oldValue = change.old_val;
 
         _.forOwn(oldValue, function (value, field) {
-          self.scServer.global.publish(query.type + '/' + query.id + '/' + field, {
-            type: 'delete',
-            value: value
+          self.scServer.global.publish(self.channelPrefix + query.type + '/' + query.id + '/' + field, {
+            type: 'delete'
           });
+        });
+      }
+
+      if (query.optimization == null) {
+        self.scServer.global.publish(self.channelPrefix + query.type, {
+          type: 'delete',
+          id: query.id
+        });
+      } else {
+        _.forOwn(viewOffsets, function (offsetData, viewName) {
+          if (self._isWithinRealtimeBounds(offsetData.offset)) {
+            self.scServer.global.publish(self._getViewChannelName(viewName, offsetData.predicateData, query.type), {
+              type: 'delete',
+              id: query.id,
+              offset: offsetData.offset
+            });
+          }
         });
       }
     }
@@ -431,23 +446,42 @@ SCCRUDRethink.prototype.delete = function (query, callback) {
   if (ModelClass == null) {
     deletedHandler('The ' + query.type + ' model type is not supported - It is not part of the schema');
   } else {
+    var tasks = [];
+
     if (query.id == null) {
       deletedHandler('Cannot delete an entire collection - ID must be provided');
     } else {
-      self._getDocumentViewOffsets(query.id, query, function (err, viewOffets) {
-        if (!err) {
-          _.forOwn(viewOffets, function (offsetData, viewName) {
-            if (self._isWithinRealtimeBounds(offsetData.offset)) {
-              self.scServer.global.publish(viewName + ':' + query.type, {
-                type: 'delete',
-                id: query.id,
-                offset: offsetData.offset
-              });
+      if (query.optimization != null) {
+        tasks.push(function (cb) {
+          self._getDocumentViewOffsets(query.id, query, cb);
+        });
+      }
+
+      if (query.field == null) {
+        tasks.push(function (cb) {
+          ModelClass.get(query.id).delete({returnChanges: true}).run(cb);
+        });
+      } else {
+        tasks.push(function (cb) {
+          ModelClass.get(query.id).run().then(function (instance) {
+            delete instance[query.field];
+            instance.save(cb);
+          }).error(cb);
+        });
+      }
+      if (tasks.length) {
+        async.series(tasks, function (err, results) {
+          if (err) {
+            deletedHandler(err);
+          } else {
+            if (query.optimization == null) {
+              deletedHandler(null, null, results[0]);
+            } else {
+              deletedHandler(null, results[0], results[1]);
             }
-          });
-        }
-        ModelClass.get(query.id).delete({returnChanges: true}).run(deletedHandler);
-      });
+          }
+        });
+      }
     }
   }
 };
